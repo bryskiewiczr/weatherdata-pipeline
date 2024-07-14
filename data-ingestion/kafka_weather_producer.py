d7cbb4dc99a44637b2bfb71f3f7f3fe8fb4abd03
rbrysk <bryskiewiczr@protonmail.com>
2024-07-14 18:44:38 +0200

    set up the rest of the infra

--- data-ingestion/kafka_weather_producer.py
+++ data-ingestion/kafka_weather_producer.py
@@ -3,23 +3,44 @@ import requests
 import json
 import time
 
-from kafka import KafkaProducer
+from kafka import KafkaProducer, KafkaAdminClient
+from kafka.errors import NoBrokersAvailable
 
 API_KEY = os.getenv("OPEN_WEATHER_MAP_API_KEY")
-CITY = os.getenv("CITY_NAME")
-KAFKA_TOPIC = os.getenv("KAFKA_WEATHER_TOPIC")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
+KAFKA_BOOTSTRAP_SERVER = "kafka:9092"
+API_URL = f"https://api.openweathermap.org/data/3.0/onecall?lat=52&lon=21&exclude=minutely,hourly,daily,alerts&appid={API_KEY}"
+
+
+def is_kafka_up() -> bool:
+    retry_timeout = 10
+    retries = 5
+    while retries > 0:
+        try:
+            admin_client = KafkaAdminClient(bootstrap_servers=KAFKA_BOOTSTRAP_SERVER)
+            return True
+        except NoBrokersAvailable:
+            retries -= 1
+            print("Kafka broker is not available, retrying...")
+            time.sleep(retry_timeout)
+    return False
 
 
 def fetch_weather_data() -> dict:
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}"
-    response = requests.get(url)
+    response = requests.get(API_URL, timeout=30)
     data = response.json()
     return data
 
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+if not is_kafka_up():
+    print("Kafka is dead, exiting...")
+    exit(1)
+
+
+PRODUCER = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVER, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
 
 while True:
     weather_data = fetch_weather_data()
-    producer.send(KAFKA_TOPIC, weather_data)
+    PRODUCER.send(KAFKA_TOPIC, weather_data)
     time.sleep(600)  # fetch the data every ten minutes
