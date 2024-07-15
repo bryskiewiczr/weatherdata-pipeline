4ceb4f3937b960661c3fff8ad40fac2def8e78b0
rbrysk <bryskiewiczr@protonmail.com>
2024-07-15 14:58:08 +0200

    hive set up properly

--- hive/hive-init.sh
+++ hive/hive-init.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sleep 30
+# Wait for Hive Metastore to be available
+while ! nc -z hive-metastore 9083; do
+  echo "Waiting for Hive Metastore to be available..."
+  sleep 2
+done
 
-hive -e "CREATE TABLE IF NOT EXISTS weather_data_hive_table (
-    temp FLOAT,
-    humidity FLOAT,
-    name STRING
-);"
+echo "Hive Metastore is available. Proceeding to create table."
+
+# Run Hive query to create table
+hive -e "CREATE TABLE IF NOT EXISTS weather_data_hive_table (temp FLOAT, humidity FLOAT, name STRING);"
